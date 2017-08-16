1a7538938f12af5187faa7d1c9ca41133244a1c8
sonjasmith <sonjasmith@outlook.com>
2017-08-15 20:50:50 -0400

    Created more variable types

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,6 +10,40 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
+            string name = "Sonja";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+
+            string birthMonth = "January";
+            //Console.WriteLine(birthMonth);
+            //integers
+            int birthMonthNumber = 1;
+            int age = 65;
+            int population = 10000;
+            
+            //Floating type variables.
+            float number = 2.164403304897294383961032f;
+            double someNumber = 2.164403304897294383961032d;
+            decimal partialNumber = 2.164403304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+
+            //Boolean
+            bool isPresent = true;
+            bool seatTaken = false;
+            
+            //Character or Char
+            char lastLetter = 'S';
+
+            int JessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(JessicaAge = samAge);
+          
         }
     }
 }
